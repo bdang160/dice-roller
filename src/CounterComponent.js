49644a7c57a515c0f9b29dbb7d9c9c55092aa46c
Bryan Dang <bdang160@gmail.com>
2021-08-11 20:08:44 -0400

    Added first button

---
+++ src/CounterComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Button from "./Button.js";
+import "./CounterComponent.css";
+
+class CounterComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrementCount = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    let { count } = this.state;
+    return (
+      <div className="counter-component">
+        <div>
+          <div class="count">
+            <h3>Count:</h3>
+            <h1>{count}</h1>
+          </div>
+          <div class="buttons">
+            <Button title={"-"} action={this.decrementCount} />
+            <Button title={"+"} action={this.incrementCount} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CounterComponent;
--- src/SiteBody.js
+++ src/SiteBody.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CounterComponent from './CounterComponent';
 import './SiteBody.css';
 
 class SiteBody extends Component {
@@ -6,6 +7,7 @@ class SiteBody extends Component {
     render () {
         return (
             <div class="site-body">
+                <CounterComponent />
             </div>
         );
     }
